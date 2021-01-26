e7fe4bb6772522f2eb37f50a2225bfcaf8e9476b
wanqi.xie <wanqi.xie@lilyenglish.com>
2021-01-26 16:40:34 +0800

    feat: 重置

--- packages/service/__tests__/service.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const service = require('..');
-
-describe('@lily/service', () => {
-    it('needs tests');
-});
--- packages/utils/lib/utils.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = utils
-console.log(1111)
-function utils() {
-  // TODO
-}
